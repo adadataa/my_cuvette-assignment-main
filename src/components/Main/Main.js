c53b73a6790fd17e3b30fbb0f8880c79decc17f4
adadataa <156297565+adadataa@users.noreply.github.com>
2024-01-11 14:02:12 +0530

    Update Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Sidebar } from '../Sidebar/Sidebar'
 import { SkillTest } from '../SkillTest/SkillTest'
 import classes from './Main.module.css'
-
+/////charo taraf hai mere hi charche bas mai aur tumhi toh ho
 export const Main = () => {
   return (
     <div className={classes.main}>
